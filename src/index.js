8342b50a5ac1088d5c2d3e8f7ad2a6f73030f7d9
gd2016 <371049740@qq.com>
2019-07-03 14:47:06 +0800

    .

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import PopBox from '../packages/popbox/index'
 import RadioBox from '../packages/radiobox/index'
 import CheckBox from '../packages/checkbox/index'
 import Alert from '../packages/Alert/index'
-import TabPgae from '../packages/tabpage/index'
+import TabPage from '../packages/tabpage/index'
 export {
   PopBox,
   RadioBox,
   CheckBox,
   Alert,
-  TabPgae
+  TabPage
 }
